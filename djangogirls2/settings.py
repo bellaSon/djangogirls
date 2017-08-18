6794a411c34439c6a0c7faa8d309a05b59ce3b3d
minjung <thsalswjd143@naver.com>
2017-08-18 18:13:30 +0900

    DB만들고 서버 최초로 띄우기

--- djangogirls2/settings.py
+++ djangogirls2/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'k70dxq9&@*80m91u!q0om#0u*5f2jjz+9l#g$svjb)b^jz6*zw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
@@ -105,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -118,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
